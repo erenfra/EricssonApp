ce7e6b0fe5b830d209706ca1f129510211412a64
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-25 21:54:25 -0600

    Initial Commit

---
+++ Ericsson/EricssonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EricssonApp.swift
+//  Ericsson
+//
+//  Created by Renato Oliveira Fraga on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct EricssonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
