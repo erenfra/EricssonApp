10a1d6b9b4e6e871942277b7d3dd06a57c2515ae
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-02-02 07:45:38 -0600

    added a naticgationstack in the detailView

--- Ericsson/Views/DetailView.swift
+++ Ericsson/Views/DetailView.swift
@@ -32,129 +32,131 @@ struct DetailView: View {
     
     var body: some View {
         
+        NavigationStack {
             VStack {
-                HStack(spacing: 30) {
-                    Image(picture ?? "N/A")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 60)
-                    Text(id)
-                        .font(.title)
-                    
-                }
-                Form {
-                    Section(header: Text("Characteristics")) {
-                        HStack(alignment: .center){
-                            Text("Bands")
-                            Spacer()
-                            ValueCard(value: "\(band)", color: .blue.opacity(0.5))
-                        }
-                        HStack(alignment: .center, spacing: 20) {
-                            Text("Dual Band")
-                            Spacer()
-                            ValueCard(value: dualBand ? "Yes" : "No", color: dualBand ? .green.opacity(0.85) : .red.opacity(0.85))
+                    HStack(spacing: 30) {
+                        Image(picture ?? "N/A")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 70)
+//                        Text(id)
+//                            .font(.title)
+                        
+                    }
+                    Form {
+                        Section(header: Text("Characteristics")) {
+                            HStack(alignment: .center){
+                                Text("Bands")
+                                Spacer()
+                                ValueCard(value: "\(band)", color: .blue.opacity(0.5))
+                            }
+                            HStack(alignment: .center, spacing: 20) {
+                                Text("Dual Band")
+                                Spacer()
+                                ValueCard(value: dualBand ? "Yes" : "No", color: dualBand ? .green.opacity(0.85) : .red.opacity(0.85))
+                             }
+                            HStack(alignment: .center) {
+                                Text("MIMO")
+                                Spacer()
+                                ValueCard(value: "\(mimo)", color: .yellow)
+                            }
+                                                 
+                            if eirp == nil {
+                                HStack(alignment: .center) {
+                                    Text("Power [W]")
+                                    Spacer()
+                                    ValueCard(value: "\(power ?? 0)", color: .mint.opacity(0.65))
+                                }
+                            } else {
+                                HStack(alignment: .center) {
+                                    Text("EIRP [dBm]")
+                                    ValueCard(value: "\(eirp ?? 0)", color: .mint.opacity(0.65))
+                                    Spacer()
+                                    Text("Antenna Gain:")
+                                    ValueCard(value: "\(antennaGain ?? 0)", color: .cyan.opacity(0.65))
+                                }
+                            }
+                            HStack(alignment: .center) {
+                                Text("Radio Group")
+                                Spacer()
+                                ValueCard(value: "\(group)", color: .purple.opacity(0.5))
+                                                   
+                            }
                          }
-                        HStack(alignment: .center) {
-                            Text("MIMO")
-                            Spacer()
-                            ValueCard(value: "\(mimo)", color: .yellow)
-                        }
-                                             
-                        if eirp == nil {
+                        Section(header: Text("Physical Characteristics")) {
+                            Toggle(isOn: $isMetric) { Text("Metric System")}
                             HStack(alignment: .center) {
-                                Text("Power [W]")
+                                Text("Width [\(isMetric ? "mm" : "inch")]")
                                 Spacer()
-                                ValueCard(value: "\(power ?? 0)", color: .mint.opacity(0.65))
+                                ValueCard(value: "\(isMetric ? width : width * 0.0394)", color: .cyan.opacity(0.5))
+                                                   
                             }
-                        } else {
                             HStack(alignment: .center) {
-                                Text("EIRP [dBm]")
-                                ValueCard(value: "\(eirp ?? 0)", color: .mint.opacity(0.65))
+                                Text("Height [\(isMetric ? "mm" : "inch")]")
                                 Spacer()
-                                Text("Antenna Gain:")
-                                ValueCard(value: "\(antennaGain ?? 0)", color: .cyan.opacity(0.65))
+                                ValueCard(value: "\(isMetric ? height : height * 0.0394)", color: .blue.opacity(0.5))
+                                                   
                             }
-                        }
-                        HStack(alignment: .center) {
-                            Text("Radio Group")
-                            Spacer()
-                            ValueCard(value: "\(group)", color: .purple.opacity(0.5))
-                                               
-                        }
-                     }
-                    Section(header: Text("Physical Characteristics")) {
-                        Toggle(isOn: $isMetric) { Text("Metric System")}
-                        HStack(alignment: .center) {
-                            Text("Width [\(isMetric ? "mm" : "inch")]")
-                            Spacer()
-                            ValueCard(value: "\(isMetric ? width : width * 0.0394)", color: .cyan.opacity(0.5))
-                                               
-                        }
-                        HStack(alignment: .center) {
-                            Text("Height [\(isMetric ? "mm" : "inch")]")
-                            Spacer()
-                            ValueCard(value: "\(isMetric ? height : height * 0.0394)", color: .blue.opacity(0.5))
-                                               
-                        }
-                        HStack(alignment: .center) {
-                            Text("Depth [\(isMetric ? "mm" : "inch")]")
-                            Spacer()
-                            ValueCard(value: "\(isMetric ? depth : depth * 0.0394)", color: .indigo.opacity(0.50))
-                                               
-                        }
-                        HStack(alignment: .center) {
-                            Text("Weight [\(isMetric ? "kg" : "pounds")]")
-                            Spacer()
-                            ValueCard(value: "\(isMetric ? weight : weight * 2.20)", color: .orange)
-                                               
-                        }
-                        
-                    }
-                    Section(header: Text("Fronthaul Connection")) {
-                        if eCpri == nil {
                             HStack(alignment: .center) {
-                                Text("CPRI [Gbps] up to")
+                                Text("Depth [\(isMetric ? "mm" : "inch")]")
                                 Spacer()
-                                ValueCard(value: "\(cpri ?? 0)", color: .blue.opacity(0.2))
+                                ValueCard(value: "\(isMetric ? depth : depth * 0.0394)", color: .indigo.opacity(0.50))
+                                                   
                             }
-                        } else {
                             HStack(alignment: .center) {
-                                Text("eCPRI [Gbps] up to")
+                                Text("Weight [\(isMetric ? "kg" : "pounds")]")
                                 Spacer()
-                                ValueCard(value: "\(eCpri ?? 0)", color: .blue.opacity(0.2))
+                                ValueCard(value: "\(isMetric ? weight : weight * 2.20)", color: .orange)
+                                                   
+                            }
+                            
+                        }
+                        Section(header: Text("Fronthaul Connection")) {
+                            if eCpri == nil {
+                                HStack(alignment: .center) {
+                                    Text("CPRI [Gbps] up to")
+                                    Spacer()
+                                    ValueCard(value: "\(cpri ?? 0)", color: .blue.opacity(0.2))
+                                }
+                            } else {
+                                HStack(alignment: .center) {
+                                    Text("eCPRI [Gbps] up to")
+                                    Spacer()
+                                    ValueCard(value: "\(eCpri ?? 0)", color: .blue.opacity(0.2))
+                                }
+                            }
+                            
+                        }
+                        Section(header: Text("Electrical")) {
+                            HStack(alignment: .center){
+                                Text("Input Power")
+                                Spacer()
+                                ValueCard(value: "\(inputPower)", color: .brown.opacity(0.5))
+                            }
+                            HStack(alignment: .center) {
+                                Text("Minimum Fuse [A]")
+                                Spacer()
+                                ValueCard(value: "\(minFuse ?? 0)", color: .green.opacity(0.5))
+                            }
+                            HStack (alignment: .center){
+                                Text("Maximun Allowed Fuse [A]")
+                                Spacer()
+                                ValueCard(value: "\(maxFuse ?? 0)", color: .yellow.opacity(0.5))
                             }
                         }
-                        
-                    }
-                    Section(header: Text("Electrical")) {
-                        HStack(alignment: .center){
-                            Text("Input Power")
-                            Spacer()
-                            ValueCard(value: "\(inputPower)", color: .brown.opacity(0.5))
-                        }
-                        HStack(alignment: .center) {
-                            Text("Minimum Fuse [A]")
-                            Spacer()
-                            ValueCard(value: "\(minFuse ?? 0)", color: .green.opacity(0.5))
-                        }
-                        HStack (alignment: .center){
-                            Text("Maximun Allowed Fuse [A]")
-                            Spacer()
-                            ValueCard(value: "\(maxFuse ?? 0)", color: .yellow.opacity(0.5))
+                        Section(header: Text("RF Connector")) {
+                            HStack(alignment: .center) {
+                                Text("Type")
+                                Spacer()
+                                ValueCard(value: "\(rfConnectorType ?? "N/A")", color: .gray.opacity(0.5))
+                            }
                         }
                     }
-                    Section(header: Text("RF Connector")) {
-                        HStack(alignment: .center) {
-                            Text("Type")
-                            Spacer()
-                            ValueCard(value: "\(rfConnectorType ?? "N/A")", color: .gray.opacity(0.5))
-                        }
-                    }
-                }
-                
-                
-                Spacer()
-            }
+                    
+                    
+                    Spacer()
+            }.navigationTitle(Text("\(id)"))
+        }
         
     }
 }
